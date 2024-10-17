bb4232a6496d1f2197b167df206abd7bab9846b1
Ha Tai Thanh <wwehttdmqvgh@gmail.com>
2024-10-18 00:21:55 +0700

    Tạo project Maven

---
+++ Quan_ly_Xuat_Nhap_canh/src/main/java/group1/quan_ly_xuat_nhap_canh/Quan_ly_Xuat_Nhap_canh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group1.quan_ly_xuat_nhap_canh;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Quan_ly_Xuat_Nhap_canh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
